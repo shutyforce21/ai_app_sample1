c525e8f62641b2b7e8db6290a2ac35481821373b
小室秀斗 <shuty@shuty-imac.local>
2020-12-31 21:51:02 +0900

    first comiit

---
+++ nlp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import pandas as pd
+import pickle
+
+category_data = pd.read_csv("idx2category.csv")
+idx2category = {row.k: row.v for idx, row in category_data.iterrows()}
+
+
+with open("rdfm.pickle", mode="rb") as f:
+    model = pickle.load(f)
+
+def index(request):
+    if request.method == "GET":
+        return render(
+            request, "nlp/home.html"
+        )
+    else:
+        title = [request.POST["title"]]
+        result = model.predict(title)[0]
+        pred = idx2category[result]
+        return render(
+            request,
+            "nlp/home.html",
+            {"title": pred}
+        )
